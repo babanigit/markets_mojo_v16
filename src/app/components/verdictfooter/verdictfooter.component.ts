287277a3456e83533f2f885de4021cac0dc1c7d9
aniket panchal <aniketpanchal07@gmail.com>
2024-09-02 11:13:44 +0530

    added

--- src/app/components/verdictfooter/verdictfooter.component.ts
+++ src/app/components/verdictfooter/verdictfooter.component.ts
@@ -1,13 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-verdictfooter',
   standalone: true,
-  imports: [],
+  imports: [
+CommonModule
+  ],
   templateUrl: './verdictfooter.component.html',
   styleUrls: ['./verdictfooter.component.css']
   // styleUrl: './verdictfooter.component.css'
 })
 export class VerdictfooterComponent {
+
+
+  stockLetters: string[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+  getStockLink(letter: string): string {
+    return `/stocks-analysis/getstocksbyname?n=${letter}`;
+  }
 
 }
