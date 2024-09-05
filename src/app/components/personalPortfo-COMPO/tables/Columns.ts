dfc7a3a0640e2399f683283d086e34e4510523a6
aniket panchal <aniketpanchal07@gmail.com>
2024-09-05 16:12:44 +0530

    some changes in table style

---
+++ src/app/components/personalPortfo-COMPO/tables/Columns.ts
@@ -0,0 +1,319 @@
+interface BaseColumn {
+  title: string;
+  code: string;
+  hold: string;
+  defaultValue: string;
+  no_extend: boolean;
+  footer: boolean;
+}
+
+export interface IColumns {
+  solo: BaseColumn[];
+  returns: BaseColumn[];
+}
+
+export const columns: IColumns = {
+  solo: [
+    //financials
+    {
+      title: 'Net Sales / Int Earned (Rs Cr) ',
+      code: 'netsale',
+      hold: '',
+      defaultValue: '-',
+      no_extend: true,
+      footer: false,
+    },
+    {
+      title: 'Other Income (Rs Cr) ',
+      code: 'othinc',
+      hold: '',
+      defaultValue: '-',
+      no_extend: true,
+      footer: false,
+    },
+    {
+      title: 'Operating Profit (Rs Cr) ',
+      code: 'opprof',
+      hold: '',
+      defaultValue: '-',
+      no_extend: true,
+      footer: false,
+    },
+    {
+      title: 'Interest Paid (Rs Cr) ',
+      code: 'intpaid',
+      hold: '%',
+      defaultValue: '-',
+      no_extend: true,
+      footer: false,
+    },
+    {
+      title: 'Tax Ratio (%)',
+      code: 'tax',
+      hold: '',
+      defaultValue: '-',
+      no_extend: true,
+      footer: false,
+    },
+    {
+      title: 'Net Profit (Rs Cr) ',
+      code: 'netprof',
+      hold: '',
+      defaultValue: '-',
+      no_extend: true,
+      footer: false,
+    },
+    {
+      title: 'EPS (Rs)',
+      code: 'eps',
+      hold: '',
+      defaultValue: '-',
+      no_extend: true,
+      footer: false,
+    },
+
+    // ratios
+    {
+      title: 'Price to Book (P/BV)',
+      code: 'pbv',
+      hold: '',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+    {
+      title: 'Return on Equity (ROE)',
+      code: 'roe',
+      hold: '',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+    {
+      title: 'Debt to Equity Ratio',
+      code: 'debeq',
+      hold: '',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+    {
+      title: 'Dividend Yield (%)',
+      code: 'divy',
+      hold: '%',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+
+    // risk
+    {
+      title: 'volatility',
+      code: 'volatility',
+      hold: '%',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+
+    {
+      title: ' Risk Adj Returns',
+      code: 'riskadj',
+      hold: '',
+      defaultValue: '0.00',
+      no_extend: true,
+      footer: false,
+    },
+
+    {
+      title: 'Beta (Adjusted)',
+      code: 'beta',
+      hold: '',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+
+    {
+      title: 'Value At Risk',
+      code: 'riskval',
+      hold: '',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+
+    // price
+    {
+      title: "Day's High",
+      code: 'dh',
+      hold: '',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: true,
+    },
+    {
+      title: "Day's Low",
+      code: 'dl',
+      hold: '',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: true,
+    },
+
+    // contribution
+    {
+      title: 'Unrealized Gain Contribution',
+      code: 'unrgaincontri',
+      hold: '%',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: true,
+    },
+    {
+      title: 'Portfolio Weight',
+      code: 'pwt',
+      hold: '%',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+
+    //dividend
+    {
+      title: 'Dividend Received',
+      code: 'div',
+      hold: '',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+
+    // tax
+    {
+      title: '31 Jan 2018',
+      code: 'jan31price',
+      hold: '',
+      defaultValue: '0.00',
+      no_extend: false,
+      footer: false,
+    },
+    {
+      title: ' Avg Holding (Yrs)',
+      code: 'avghold',
+      hold: '',
+      defaultValue: '0.00',
+      no_extend: false,
+      footer: true,
+    },
+    {
+      title: ' ST Tax (If Sold)',
+      code: 'sttax',
+      hold: '',
+      defaultValue: '-',
+      no_extend: false,
+      footer: true,
+    },
+    {
+      title: 'Days left for ST Tax',
+      code: 'dayleft',
+      hold: '',
+      defaultValue: 'Tax Free',
+      no_extend: false,
+      footer: false,
+    },
+    {
+      title: ' LT Tax (If Sold)',
+      code: 'lttax',
+      hold: '',
+      defaultValue: '-',
+      no_extend: false,
+      footer: true,
+    },
+    {
+      title: ' Post Tax Value',
+      code: 'ptv',
+      hold: '',
+      defaultValue: '-',
+      no_extend: false,
+      footer: true,
+    },
+  ],
+  returns: [
+    {
+      title: '1 Day ',
+      code: 'D1',
+      hold: '%',
+      defaultValue: '-',
+      no_extend: true,
+      footer: false,
+    },
+    {
+      title: '1 Week  ',
+      code: 'W1',
+      hold: '%',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+    {
+      title: '1 Month   ',
+      code: 'M1',
+      hold: '%',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+    {
+      title: '3 Months ',
+      code: 'M3',
+      hold: '%',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+    {
+      title: '6 Months ',
+      code: 'M6',
+      hold: '%',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+    {
+      title: 'YTD ',
+      code: 'YTD',
+      hold: '%',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+
+    {
+      title: '1 Year  ',
+      code: 'Y1',
+      hold: '%',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+
+    {
+      title: '3 Year  ',
+      code: 'Y3',
+      hold: '%',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+
+    {
+      title: '5 Year ',
+      code: 'Y5',
+      hold: '%',
+      defaultValue: 'NA',
+      no_extend: true,
+      footer: false,
+    },
+  ],
+};
--- src/app/components/personalPortfo-COMPO/tables/tables.component.ts
+++ src/app/components/personalPortfo-COMPO/tables/tables.component.ts
@@ -20,6 +20,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { PopupComponent } from '../../others/popup/popup.component';
 
+import { columns, IColumns } from './Columns';
+
 @Component({
   selector: 'app-tables',
   templateUrl: './tables.component.html',
@@ -42,312 +44,6 @@ import { PopupComponent } from '../../others/popup/popup.component';
 
 // , AfterViewInit
 export class TablesComponent implements OnInit {
-
-  return=[
-    {
-      title: 'M1 ',
-      code: 'M1',
-      hold: '',
-      defaultValue: '-',
-      no_extend: true,
-      footer: false,
-    },
-    // {
-    //   title: 'Net Sales / Int Earned (Rs Cr) ',
-    //   code: 'netsale',
-    //   hold: '',
-    //   defaultValue: '-',
-    //   no_extend: true,
-    //   footer: false,
-    // },
-    // {
-    //   title: 'Net Sales / Int Earned (Rs Cr) ',
-    //   code: 'netsale',
-    //   hold: '',
-    //   defaultValue: '-',
-    //   no_extend: true,
-    //   footer: false,
-    // },
-    // {
-    //   title: 'Net Sales / Int Earned (Rs Cr) ',
-    //   code: 'netsale',
-    //   hold: '',
-    //   defaultValue: '-',
-    //   no_extend: true,
-    //   footer: false,
-    // },
-    // {
-    //   title: 'Net Sales / Int Earned (Rs Cr) ',
-    //   code: 'netsale',
-    //   hold: '',
-    //   defaultValue: '-',
-    //   no_extend: true,
-    //   footer: false,
-    // },
-    // {
-    //   title: 'Net Sales / Int Earned (Rs Cr) ',
-    //   code: 'netsale',
-    //   hold: '',
-    //   defaultValue: '-',
-    //   no_extend: true,
-    //   footer: false,
-    // },
-    // {
-    //   title: 'Net Sales / Int Earned (Rs Cr) ',
-    //   code: 'netsale',
-    //   hold: '',
-    //   defaultValue: '-',
-    //   no_extend: true,
-    //   footer: false,
-    // },
-    // {
-    //   title: 'Net Sales / Int Earned (Rs Cr) ',
-    //   code: 'netsale',
-    //   hold: '',
-    //   defaultValue: '-',
-    //   no_extend: true,
-    //   footer: false,
-    // },
-    // {
-    //   title: 'Net Sales / Int Earned (Rs Cr) ',
-    //   code: 'netsale',
-    //   hold: '',
-    //   defaultValue: '-',
-    //   no_extend: true,
-    //   footer: false,
-    // },
-
-
-  ]
-
-  solo = [
-
-    //financials
-    {
-      title: 'Net Sales / Int Earned (Rs Cr) ',
-      code: 'netsale',
-      hold: '',
-      defaultValue: '-',
-      no_extend: true,
-      footer: false,
-    },
-    {
-      title: 'Other Income (Rs Cr) ',
-      code: 'othinc',
-      hold: '',
-      defaultValue: '-',
-      no_extend: true,
-      footer: false,
-    },
-    {
-      title: 'Operating Profit (Rs Cr) ',
-      code: 'opprof',
-      hold: '',
-      defaultValue: '-',
-      no_extend: true,
-      footer: false,
-    },
-    {
-      title: 'Interest Paid (Rs Cr) ',
-      code: 'intpaid',
-      hold: '%',
-      defaultValue: '-',
-      no_extend: true,
-      footer: false,
-    },
-    {
-      title: 'Tax Ratio (%)',
-      code: 'tax',
-      hold: '',
-      defaultValue: '-',
-      no_extend: true,
-      footer: false,
-    },
-    {
-      title: 'Net Profit (Rs Cr) ',
-      code: 'netprof',
-      hold: '',
-      defaultValue: '-',
-      no_extend: true,
-      footer: false,
-    },
-    {
-      title: 'EPS (Rs)',
-      code: 'eps',
-      hold: '',
-      defaultValue: '-',
-      no_extend: true,
-      footer: false,
-    },
-
-    // ratios
-    {
-      title: 'Price to Book (P/BV)',
-      code: 'pbv',
-      hold: '',
-      defaultValue: 'NA',
-      no_extend: true,
-      footer: false,
-    },
-    {
-      title: 'Return on Equity (ROE)',
-      code: 'roe',
-      hold: '',
-      defaultValue: 'NA',
-      no_extend: true,
-      footer: false,
-    },
-    {
-      title: 'Debt to Equity Ratio',
-      code: 'debeq',
-      hold: '',
-      defaultValue: 'NA',
-      no_extend: true,
-      footer: false,
-    },
-    {
-      title: 'Dividend Yield (%)',
-      code: 'divy',
-      hold: '%',
-      defaultValue: 'NA',
-      no_extend: true,
-      footer: false,
-    },
-
-    // risk
-    {
-      title: 'volatility',
-      code: 'volatility',
-      hold: '%',
-      defaultValue: 'NA',
-      no_extend: true,
-      footer: false,
-    },
-
-    {
-      title: ' Risk Adj Returns',
-      code: 'riskadj',
-      hold: '',
-      defaultValue: '0.00',
-      no_extend: true,
-      footer: false,
-    },
-
-    {
-      title: 'Beta (Adjusted)',
-      code: 'beta',
-      hold: '',
-      defaultValue: 'NA',
-      no_extend: true,
-      footer: false,
-    },
-
-    {
-      title: 'Value At Risk',
-      code: 'riskval',
-      hold: '',
-      defaultValue: 'NA',
-      no_extend: true,
-      footer: false,
-    },
-
-    // price
-    {
-      title: "Day's High",
-      code: 'dh',
-      hold: '',
-      defaultValue: 'NA',
-      no_extend: true,
-      footer: true,
-    },
-    {
-      title: "Day's Low",
-      code: 'dl',
-      hold: '',
-      defaultValue: 'NA',
-      no_extend: true,
-      footer: true,
-    },
-
-    // contribution
-    {
-      title: 'Unrealized Gain Contribution',
-      code: 'unrgaincontri',
-      hold: '%',
-      defaultValue: 'NA',
-      no_extend: true,
-      footer: true,
-    },
-    {
-      title: 'Portfolio Weight',
-      code: 'pwt',
-      hold: '%',
-      defaultValue: 'NA',
-      no_extend: true,
-      footer: false,
-    },
-
-    //dividend
-    {
-      title: 'Dividend Received',
-      code: 'div',
-      hold: '',
-      defaultValue: 'NA',
-      no_extend: true,
-      footer: false,
-    },
-
-    // tax
-    {
-      title: '31 Jan 2018',
-      code: 'jan31price',
-      hold: '',
-      defaultValue: '0.00',
-      no_extend: false,
-      footer: false,
-    },
-    {
-      title: ' Avg Holding (Yrs)',
-      code: 'avghold',
-      hold: '',
-      defaultValue: '0.00',
-      no_extend: false,
-      footer: true,
-    },
-    {
-      title: ' ST Tax (If Sold)',
-      code: 'sttax',
-      hold: '',
-      defaultValue: '-',
-      no_extend: false,
-      footer: true,
-    },
-    {
-      title: 'Days left for ST Tax',
-      code: 'dayleft',
-      hold: '',
-      defaultValue: 'Tax Free',
-      no_extend: false,
-      footer: false,
-    },
-    {
-      title: ' LT Tax (If Sold)',
-      code: 'lttax',
-      hold: '',
-      defaultValue: '-',
-      no_extend: false,
-      footer: true,
-    },
-    {
-      title: ' Post Tax Value',
-      code: 'ptv',
-      hold: '',
-      defaultValue: '-',
-      no_extend: false,
-      footer: true,
-    },
-  ];
-
   // private _liveAnnouncer = inject(LiveAnnouncer);
   private serv = inject(GetPersonalPFService);
 
@@ -376,6 +72,12 @@ export class TablesComponent implements OnInit {
     | 'TOTAL_RETURNS' = 'HOLDING';
 
   private dataCache: { [key: string]: any[] | undefined } = {};
+
+  col: IColumns;
+
+  constructor() {
+    this.col = columns;
+  }
 
   ngOnInit() {
     this.getColums('HOLDING');
@@ -427,19 +129,21 @@ export class TablesComponent implements OnInit {
       | 'RESULTS'
       | 'TOTAL_RETURNS'
   ) {
-
-    // if (this.dataCache[type]) {
-    //   // console.log('the data cache is : ', this.dataCache[type]);
-    //   this.updateStocks(type);
-    //   return;
-    // }
+    if (this.dataCache[type]) {
+      // console.log('the data cache is : ', this.dataCache[type]);
+      this.updateStocks(type);
+      return;
+    }
 
     this.serv.getOverviewStocks(type).subscribe({
       next: (response) => {
-
-
         let elements;
-        if (type === 'RISK' || type === 'RATIOS' || type === 'FINANCIALS' || type === 'RETURNS' ) {
+        if (
+          type === 'RISK' ||
+          type === 'RATIOS' ||
+          type === 'FINANCIALS' ||
+          type === 'RETURNS'
+        ) {
           elements = Object.values(response.data);
         } else {
           elements = Object.values(response.data.list);
@@ -574,7 +278,7 @@ export class TablesComponent implements OnInit {
           'score',
           'cmp',
           'mcap',
-          'Y1',
+          // 'Y1',
           'volatility',
           'riskadj',
           'beta',
@@ -639,8 +343,17 @@ export class TablesComponent implements OnInit {
         break;
 
       case 'RETURNS':
-        this.displayedColumns = ['short', 'score', 'cmp'
-          // , 'D1'
+        this.displayedColumns = [
+          'short',
+          'score',
+          'cmp',
+          'D1',
+          'W1',
+          'M1',
+          'M3',
+          'Y1',
+          'Y3',
+          'Y5',
         ];
         break;
       case 'RESULTS':
@@ -689,7 +402,6 @@ export class TablesComponent implements OnInit {
   }
 
   private sortData(sortState: Sort) {
-
     console.log('sortData called ...', sortState);
 
     // Create a copy of the data to sort
@@ -723,7 +435,11 @@ export class TablesComponent implements OnInit {
         }
       }
 
-      if ( (this.TYPE !== 'RISK') && (sortState.active === 'D1' || sortState.active === 'Y1')) {
+      if (
+        this.TYPE !== 'RISK' &&
+        // sortState.active === 'D1' || sortState.active === 'Y1'
+        ['D1', 'W1', 'M1', 'M3', 'Y1', 'Y3', 'Y5'].includes(sortState.active)
+      ) {
         valueA = a.returns?.[sortState.active]?.val;
         valueB = b.returns?.[sortState.active]?.val;
       } else if (a.dotsum && b.dotsum) {
